3ef344cafc736457d4d1c7fae62a7cc2ed5e269d
ChanggunLee <cd8114@gmail.com>
2018-06-24 13:31:04 +0900

    chage code

--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
+const catchErrors = require('../lib/async-error');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
+/* GET home page. */
+router.get('/', catchErrors( async( req, res, next ) => {
   res.send('respond with a resource');
-});
-
+}));
 module.exports = router;
