e242e7b673f24e96d94cc35a938b7f28d1bdad66
ChanggunLee <cd8114@gmail.com>
2018-06-24 12:18:01 +0900

    test app.js

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(sassMiddleware({
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
