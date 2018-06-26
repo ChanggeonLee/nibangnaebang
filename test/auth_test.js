81ecc6af90e2642aa7658e86b80e01f778762079
ChanggunLee <cd8114@gmail.com>
2018-06-26 22:45:22 +0900

    add bugfix

--- test/auth_test.js
+++ test/auth_test.js
@@ -1,17 +1,34 @@
 const assert = require("assert"); //nodejs에서 제공하는 aseert 모듈
+const httpMocks = require('node-mocks-http');
 const app = require('../app.js');
 const request = require('supertest');
 
-describe('test auth', function() {
-  it('path /auth .', function(done) {
-    request(app)
-      .get('/auth')
-      .expect(200,done)
-      .end((err, res) => {
-          if (err) throw err;
-          done();
-      })
+describe('test - user.js', function() {
+  before(function() {
+    // excuted before test suite
+  });
+
+  after(function() {
+    // excuted after test suite
+  });
+
+  beforeEach(function() {
+    // excuted// excuted before every test
+  });
+
+  afterEach(function() {
+    // excuted after every test
+  });
+
+  describe('test users', function() {
+    it('path /users .', function(done) {
+      request(app)
+        .get('/')
+        .expect(200)
+        .end((err, res) => {
+            if (err) throw err;
+            done();
+        })
+    });
   });
 });
-
-
