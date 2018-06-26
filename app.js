d98543b84f85a02d50f8d8fd9ccae4eb8f47d43f
ChanggunLee <cd8114@gmail.com>
2018-06-26 21:15:59 +0900

    css error

--- app.js
+++ app.js
@@ -38,7 +38,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
 app.use(sassMiddleware({
   src: path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
