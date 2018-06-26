6348e0d2863ada729cde01862e9755d38fcd1123
jaehyun0415 <cant147@naver.com>
2018-06-26 21:57:02 +0900

    fixed signin

--- routes/auth.js
+++ routes/auth.js
@@ -4,16 +4,12 @@ const catchErrors = require('../lib/async-error');
 
 /* GET home page. */
 router.get('/', catchErrors( async( req, res, next ) => {
-<<<<<<< HEAD
   res.render('index');
-=======
-    res.render('signin/index');
->>>>>>> 94ef1196262f18cee678518a8a25ecf563f2cbe7
 }));
 
 /* GET home page. */
 router.get('/signin', catchErrors( async( req, res, next ) => {
-  res.render('index');
+  res.render('signin/index');
 }));
 
 // mypage
