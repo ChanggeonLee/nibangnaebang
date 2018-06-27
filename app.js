27cbab500c3b9dc0fcc919bf62e1a0747fb8e5a4
ChanggunLee <cd8114@gmail.com>
2018-06-27 21:02:30 +0900

    bugfix scss

--- app.js
+++ app.js
@@ -36,8 +36,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(sassMiddleware({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
+  src: path.join(__dirname, '/public'),
+  dest: path.join(__dirname, '/public'),
   indentedSyntax: false, // true = .sass and false = .scss
   sourceMap: true
 }));
