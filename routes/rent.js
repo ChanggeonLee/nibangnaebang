7082f60ada2d3b65ceac8bd531817f0757e43976
ChanggunLee <cd8114@gmail.com>
2018-06-28 16:13:35 +0900

    add rent upload

--- routes/rent.js
+++ routes/rent.js
@@ -6,8 +6,11 @@ router.get('/', catchErrors( async( req, res, next ) => {
   res.render('rent/index');
 }));
 
-router.get('/rent_detail', catchErrors( async( req, res, next) => {
+router.get('/detail', catchErrors( async( req, res, next) => {
   res.render('rent_detail/index');
 }));
 
+router.get('/upload', catchErrors( async (req , res, next) => {
+  res.render('rent_upload/index');
+}));
 module.exports = router;
