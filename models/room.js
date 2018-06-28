d94870ae6cb7745a7cc47ff13720e332fc133cb3
ChanggunLee <cd8114@gmail.com>
2018-06-28 19:48:12 +0900

    upload post

--- models/room.js
+++ models/room.js
@@ -9,19 +9,19 @@ var schema = new Schema({
     //건물이름, 호수
     detail_address: {type: String, required: true, trim: false},
     // 기간
-    start_time: {
-        date:{type: String, required: true, trim: true},
-        time:{type: String, required: true, trim: true}
-    },
-    end_time: {
-        date:{type: String, required: true, trim: true},
-        time:{type: String, required: true, trim: true}
-    },
+    // start_time: {
+    //     date:{type: String, required: true, trim: true},
+    //     time:{type: String, required: true, trim: true}
+    // },
+    // end_time: {
+    //     date:{type: String, required: true, trim: true},
+    //     time:{type: String, required: true, trim: true}
+    // },
     // 적정 인원
     suitable_person: {type: String, required: true, trim: true},
-    option: {type: Boolean, required : true, default: false},
-    info: {type: String, required: true, trim: false},
-    sell : {type: Boolean, required: true, default: false},
+    // option: {type: Boolean, required : true, default: false},
+    // info: {type: String, required: true, trim: false},
+    // sell : {type: Boolean, required: true, default: false},
     // 좋아요 수
     numLikes: {type: Number, default: 0},
     img: [String],
--- routes/rent.js
+++ routes/rent.js
@@ -33,5 +33,14 @@ router.get('/upload/:id',needAuth, catchErrors( async (req , res, next) => {
 // 방 올리기 post
 router.post('/upload/:id', needAuth ,catchErrors( async (req , res, next ) => {
   console.log(req.body);
+  var rent = new Room({
+    author: req.user.id,
+    locate: req.body.locate,
+    detail_address: req.body.detail_address,
+    suitable_person: req.body.suitable_person
+  });
+  await rent.save();
+  console.log("니방 올리기 성공");
+  res.redirect('/rent');
 }));
 module.exports = router;
