2bfea91976f0157ac0bd7c8fbc9938a0493d1dd6
ChanggunLee <cd8114@gmail.com>
2018-06-28 20:11:50 +0900

    aws s3

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,32 @@ router.get('/', catchErrors( async( req, res, next ) => {
     res.render('index', { title: '똥땅이네'});
 }));
 
+
+// AWS 이미지 업로드
+const S3_BUCKET = process.env.S3_BUCKET;
+router.get('/s3', function(req, res, next) {
+  const s3 = new aws.S3({region: 'ap-northeast-2'});
+  const filename = req.query.filename;
+  const type = req.query.type;
+  const uuid = uuidv4();
+  const params = {
+    Bucket: S3_BUCKET,
+    Key: uuid + '/' + filename,
+    Expires: 900,
+    ContentType: type,
+    ACL: 'public-read'
+  };
+  console.log(params);
+  s3.getSignedUrl('putObject', params, function(err, data) {
+    if (err) {
+      console.log(err);
+      return res.json({err: err});
+    }
+    res.json({
+      signedRequest: data,
+      url: `https://${S3_BUCKET}.s3.amazonaws.com/${uuid}/${filename}`
+    });
+  });
+});
+
 module.exports = router;
