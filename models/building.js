6aaf89b9734c9a6df4df8b91fd88a1e65df64aaa
unknown <jaelee24@naver.com>
2018-06-30 00:07:43 +0900

    review database initialized

--- models/building.js
+++ models/building.js
@@ -6,15 +6,7 @@ var schema = new Schema({
     //지역
     locate: {type: String, required: true, trim: true},
     //건물이름
-    detail_address: {type: String, required: true, trim: false},
-
-    
-
-
-  
-    // 평균별점
-    star_avg: {type: Number, default: 0},
-    img: [String],
+    building_name: {type: String, required: true, trim: false}
 }, {
   toJSON: { virtuals: true},
   toObject: {virtuals: true}
