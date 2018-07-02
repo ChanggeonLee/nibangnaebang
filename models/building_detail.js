b6e9fe7efa873e02ddc354ea7308675f4e216d00
ChanggunLee <cd8114@gmail.com>
2018-07-02 21:02:00 +0900

    add comment in detail comment

---
+++ models/building_detail.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const mongoosePaginate = require('mongoose-paginate');
+const Schema = mongoose.Schema;
+
+var schema = new Schema({
+   // 건물 이름
+   building_name: {type: String},
+   img: {type: String},
+}, {
+  toJSON: { virtuals: true},
+  toObject: {virtuals: true}
+});
+
+schema.plugin(mongoosePaginate);
+var building_detail = mongoose.model('building_detail', schema);
+
+module.exports = building_detail;
+
--- routes/review.js
+++ routes/review.js
@@ -2,7 +2,10 @@ var express = require('express');
 var router = express.Router();
 const catchErrors = require('../lib/async-error');
 
+// D.B
 var Building = require('../models/building');
+var Building_detail = require('../models/building_detail');
+var Comment = require('../models/comment');
 
 const needAuth = require('../lib/need-auth');
 
@@ -13,7 +16,10 @@ router.get('/', catchErrors( async( req, res, next ) => {
 }));
 
 router.get('/detail/:id',catchErrors( async( req, res , next) => {
-  res.render('review_detail/index');
+  building_detail = await Building_detail.findOne({ building_name : req.params.id });
+  comments = await Comment.find({building_detail : building_detail._id});
+
+  res.render('review_detail/index', {building_detail:building_detail , comments:comments});
 }));
 
 module.exports = router;
