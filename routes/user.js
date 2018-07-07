851871cf66ecbc321214f3df1f7bf97dd6adc868
ChanggunLee <cd8114@gmail.com>
2018-07-07 23:35:09 +0900

    bug fix in mypage

--- routes/user.js
+++ routes/user.js
@@ -5,7 +5,6 @@ const catchErrors = require('../lib/async-error');
 // 필요한 디비 require
 var User = require('../models/user');
 var LikeLog = require('../models/like-log');
-var Rent =require('../models/rent');
 
 // 로그인 확인
 const needAuth = require('../lib/need-auth');
@@ -135,6 +134,7 @@ router.delete('/:id' ,needAuth , catchErrors( async(req,res,next) => {
 router.get('/mypage/:id',needAuth, catchErrors( async( req, res, next ) => {
   var user = await User.findById(req.params.id);
   var like_logs = await LikeLog.find({author : req.params.id}).populate('rent');
+  console.log(like_logs);
   res.render('./mypage/index' , {user:user , like_logs:like_logs});
 }));
 
