2167bdbaca3b708557147f142e2927e2157b2f84
ChanggunLee <cd8114@gmail.com>
2018-07-07 21:50:57 +0900

    recipe

--- models/recipe_default.js
+++ models/recipe_default.js
@@ -1,16 +1,31 @@
 const mongoose = require('mongoose');
-//const mongoosePaginate = require('mongoose-paginate');
+const mongoosePaginate = require('mongoose-paginate');
 const Schema = mongoose.Schema;
 
 var schema = new Schema({
-    title: [{type:String}],
-    data: [{type: String}]
+    data: [{DET_URL: String, 
+            IRDNT_CODE: String, 
+            IMG_URL: String,
+            NATION_CODE: String,
+            SUMRY: String,
+            CALORIE: String,
+            TY_CODE: String,
+            RECIPE_NM_KO: String,
+            RN: Number,
+            QNT: String,
+            PC_NM: String,
+            TY_NM: String,
+            LEVE_NM: String,
+            RECIPE_ID: Number,
+            NATION_NM: String,
+            COOKING_TIME: String
+          }]
 }, {
     toJSON: { virtuals: true},
     toObject: {virtuals: true}
 });
 
-//schema.plugin(mongoosePaginate);
+schema.plugin(mongoosePaginate);
 var recipe_default = mongoose.model('default', schema);
 
 module.exports = recipe_default;
--- routes/recipe.js
+++ routes/recipe.js
@@ -2,11 +2,19 @@ var express = require('express');
 var router = express.Router();
 const catchErrors = require('../lib/async-error');
 
-const recipe = require('../models/recipe_default');
+const Recipe_default = require('../models/recipe_default');
 
 /* GET home page. */
 router.get('/', catchErrors( async( req, res, next ) => {
-  res.render('recipe/index');
-  console.log(recipe);
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 5;
+
+  var recipe_default = await Recipe_default.paginate({ 
+    sort: {RECIPE_ID:1}, 
+    page: page, limit: limit
+  });
+
+  // console.log(recipe_default.data);
+  res.render('recipe/index' , {recipe_default:recipe_default });
 }));
 module.exports = router;
