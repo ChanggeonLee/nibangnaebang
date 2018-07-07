2ca5b1e09e41889b1a33026517fb7689a164324b
ChanggunLee <cd8114@gmail.com>
2018-07-07 23:43:40 +0900

    bug fix pageinate

--- routes/recipe.js
+++ routes/recipe.js
@@ -7,17 +7,17 @@ const Recipe_default = require('../models/recipe_default');
 /* GET home page. */
 router.get('/', catchErrors( async( req, res, next ) => {
   const page = parseInt(req.query.page) || 1;
-  const limit = parseInt(req.query.limit) || 5;
+  const limit = parseInt(req.query.limit) || 6;
 
   var query = {};
-  // const term = req.query.term;
-  // // if (term) {
-  // //   query = {$or: [
-  // //     {title: {'$regex': term, '$options': 'i'}},
-  // //     {locate: {'$regex': term, '$options': 'i'}},
-  // //     {event_field: {'$regex': term, '$options': 'i'}}
-  // //   ]};
-  // // }
+  const term = req.query.term;
+  if (term) {
+    query = {$or: [
+      // {title: {'$regex': term, '$options': 'i'}},
+      // {locate: {'$regex': term, '$options': 'i'}},
+      // {event_field: {'$regex': term, '$options': 'i'}}
+    ]};
+  }
 
   const recipe_default = await Recipe_default.paginate(query, {
     sort: {RECIPE_ID: 1}, 
